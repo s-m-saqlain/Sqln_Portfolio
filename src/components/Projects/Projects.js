15dc25207f492238744c04561e8d7dd89c94a8b5
saqlainhnh <mohd.313saqlain@gmail.com>
2024-11-24 18:31:53 +0500

    Portfolio

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -62,7 +62,7 @@ function Projects() {
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
-              title="Plant AI"
+              title="Construction"
               description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
               // ghLink="https://github.com/soumyajit4419/Plant_AI"
               demoLink="https://construction-gamma-two.vercel.app/"
